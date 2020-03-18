8103b0ea9f13cf9280433cf8b4f6ac37164a1a7a
Petr Procházka <petrprochy@gmail.com>
2020-03-18 15:29:56 +0100

    Updated security realm via pam-auth

--- src/main/java/hudson/plugins/pwauth/PWauthSecurityRealm.java
+++ src/main/java/hudson/plugins/pwauth/PWauthSecurityRealm.java
@@ -1,18 +1,26 @@
 package hudson.plugins.pwauth;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
+import hudson.Extension;
+import hudson.Functions;
 import hudson.security.AbstractPasswordBasedSecurityRealm;
 import hudson.security.GroupDetails;
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterConfig;
 import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.BadCredentialsException;
 import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.GrantedAuthorityImpl;
 import org.acegisecurity.userdetails.User;
 import org.acegisecurity.userdetails.UserDetails;
-import org.acegisecurity.userdetails.UserDetailsService;
 import org.acegisecurity.userdetails.UsernameNotFoundException;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.springframework.dao.DataAccessException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /** 
@@ -52,47 +60,29 @@ public class PWauthSecurityRealm extends AbstractPasswordBasedSecurityRealm {
 		if (PWauthValidation.validatePath(idPath))
 			PWauthUtils.setIdPath(idPath);
 	}
-	
-	@Override
-	public SecurityComponents createSecurityComponents() {
-		return new SecurityComponents(
-			new PWauthAthenticationManager(),
-			new UserDetailsService() {
-				public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
-					try {
-						if (PWauthUtils.userExists(username))
-							return new User(username, "", true, true, true, true,
-								new GrantedAuthority[] { AUTHENTICATED_AUTHORITY });
-					} catch (IOException e) {}
-					throw new UsernameNotFoundException("No such Unix user: " + username);
-				}
-			});
-	}
-	
+
 	@Override
 	public UserDetails authenticate(String username, String password) throws AuthenticationException {
 		try {
 			if (PWauthUtils.isUserValid(username, password))
 				return new User(username, "", true, true, true, true,
-					new GrantedAuthority[] { AUTHENTICATED_AUTHORITY });
+						toAuthorities(username));
 		} catch (Exception e) {
-			throw new AuthenticationException("User could not be authenticated", e) {
-				private static final long serialVersionUID = 8636276439158457192L;
-			};
+			throw new BadCredentialsException("User could not be authenticated", e);
 		}
 		return null;
 	}
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
 		try {
 			if (PWauthUtils.userExists(username))
 				return new User(username, "", true, true, true, true,
-					new GrantedAuthority[] { AUTHENTICATED_AUTHORITY });
-		} catch (IOException e) {}
+						toAuthorities(username));
+		} catch (IOException ignored) {}
 		throw new UsernameNotFoundException("No such Unix user: " + username);
 	}
-	
+
 	@Override
 	public GroupDetails loadGroupByGroupname(final String groupname) throws UsernameNotFoundException, DataAccessException {
 		if (PWauthUtils.groupExists(groupname))
@@ -104,9 +94,26 @@ public class PWauthSecurityRealm extends AbstractPasswordBasedSecurityRealm {
 			}
 		};
 	}
-	
+
 	@Override
 	public Filter createFilter(FilterConfig filterConfig) {
 		return new PWauthFilter(super.createFilter(filterConfig), this);
+	}
+
+	private static GrantedAuthority[] toAuthorities(String userName) throws IOException {
+		final List<String> userGroups = PWauthUtils.getUserGroups(userName);
+		final List<GrantedAuthority> authorities = new ArrayList<>(userGroups.size() + 1);
+		//noinspection ConstantConditions
+		authorities.addAll(Collections2.transform(
+				userGroups, (Function<String, GrantedAuthority>) GrantedAuthorityImpl::new));
+		authorities.add(AUTHENTICATED_AUTHORITY);
+		return authorities.toArray(new GrantedAuthority[0]);
+	}
+
+	@SuppressWarnings("unused")
+	@Extension
+	public static PWauthDescriptor install() {
+		if (!Functions.isWindows()) return new PWauthDescriptor();
+		return null;
 	}
 }
